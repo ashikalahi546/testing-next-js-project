3d35600bbf10c36f38e1dd215abc11c5a6740189
ashikalahi546 <144088399+ashikalahi546@users.noreply.github.com>
2024-07-13 22:24:10 +0600

    Update DataFetchingTwo.jsx

--- src/components/DataFetchingTwo.jsx
+++ src/components/DataFetchingTwo.jsx
@@ -24,7 +24,7 @@ const DataFetchingTwo = () => {
 
 
   const handleLoader =async ()=>{
-setLoading(true)
+setButtonLoading(true)
 try{
 const res = await fetch('https://jsonplaceholder.typicode.com/comments')
 const result = await res.json()
