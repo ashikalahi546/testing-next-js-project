5aea3af1fdd35e98292e89e108909ceab835057d
ashikalahi546 <144088399+ashikalahi546@users.noreply.github.com>
2024-10-26 23:57:34 +0600

    Create CardData.js

---
+++ src/components/allData/CardData.js
@@ -0,0 +1,394 @@
+export const cardData = [
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "corporate",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "corporate",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "corporate",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "corporate",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "corporate",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "corporate",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "corporate",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "corporate",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "corporate",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "corporate",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+
+
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "corporate",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "corporate",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "homeplan",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "homeplan",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "homeplan",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "homeplan",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "homeplan",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "homeplan",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "homeplan",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "homeplan",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "homeplan",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "homeplan",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "homeplan",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "homeplan",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "corporate",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+    {
+        "plan_id": 102,
+        "name": "Premium Plan",
+        "speed": "150 Mbps",
+        "data_limit": "500 GB",
+        "price": "$49.99",
+        "contract_period": "12 months",
+        "type": "corporate",
+        "items": [
+            { name: "Unlimited voice calls", action: true },
+            { name: "Free modem and installation", action: true },
+            { name: "Priority customer support", action: false },
+            { name: "Access to streaming services", action: false }
+        ],
+    },
+];
